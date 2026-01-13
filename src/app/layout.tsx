6d42814c645945b6ca1034ad2242262c24f5e579
atikur-24 <atikurrahman7223@gmail.com>
2026-01-13 18:04:34 +0600

    project setup done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,10 @@
+import Navbar from "@/components/navbar/Navbar";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Public_Sans } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const publicSans = Public_Sans({
+  variable: "--font-public-sans",
   subsets: ["latin"],
 });
 
@@ -24,9 +20,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${publicSans.variable} antialiased`}>
+        <Navbar />
         {children}
       </body>
     </html>
